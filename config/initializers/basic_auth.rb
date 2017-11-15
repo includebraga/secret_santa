3bd652ed54922059a858945b411b4948d3a99152
Fernando Mendes <devfrmendes@gmail.com>
2017-11-15 10:54:05 +0000

    HOTFIX: Basic auth settings

--- config/initializers/basic_auth.rb
+++ config/initializers/basic_auth.rb
@@ -1,5 +1,7 @@
-if %i(production staging).include?(Rails.env) && ENV["BASIC_AUTH"].present?
-  config.middleware.use '::Rack::Auth::Basic' do |u, p|
-    [username, password] == ENV["BASIC_AUTH"].split(":")
+if %w(production staging).include?(Rails.env) && ENV["BASIC_AUTH"].present?
+  Rails.application.configure do
+    config.middleware.use Rack::Auth::Basic do |username, password|
+      [username, password] == ENV["BASIC_AUTH"].split(":")
+    end
   end
 end
