52c7ae32d71fb00cbe99305a1b59b0b76a1716de
Fernando Mendes <devfrmendes@gmail.com>
2017-11-16 17:19:17 +0000

    HOTFIX: Separate mailer host from domain

--- config/initializers/mailer.rb
+++ config/initializers/mailer.rb
@@ -10,7 +10,7 @@ if %w(production staging).include?(Rails.env)
   ActionMailer::Base.smtp_settings = {
     user_name: ENV.fetch("MAILER_USERNAME"),
     password: ENV.fetch("MAILER_PASSWORD"),
-    domain: ENV.fetch("MAILER_HOST"),
+    domain: ENV.fetch("MAILER_DOMAIN"),
     address: ENV.fetch("MAILER_ADDRESS"),
     port: ENV.fetch("MAILER_PORT").to_i,
     :authentication => :plain,
