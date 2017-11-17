ec53d869e3c0cd74c4173ef6dd651077050ebc59
Fernando Mendes <devfrmendes@gmail.com>
2017-11-17 20:02:35 +0000

    Update routes to match new content

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,19 +1,4 @@
 class HomeController < ApplicationController
   def index
-    @user = user_from_session
-  end
-
-  private
-
-  def user_from_session
-    if user_params
-      User.new(user_params).with_validations
-    else
-      User.new
-    end
-  end
-
-  def user_params
-    @_user_params ||= session.delete(:user)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root "home#index"
 
-  resources :users, only: [:create]
+  resources :users, only: [:create, :new]
 
   namespace :users do
     get "/confirm/:token", to: "confirmations#create", as: :confirmation
