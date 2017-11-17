72ad0d7ce4a77e72f0e7f60fb19f877757adf9ce
9thScientist <bruno.ms.cancelinha@gmail.com>
2017-11-17 20:18:25 +0000

    Adds administrate

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    resources :users
+
+    root to: "users#index"
+  end
+
   root "home#index"
 
   resources :users, only: [:create, :new]
