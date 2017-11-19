85482e07b268fd9a92ca23ed6204bdbd534a1142
Fernando Mendes <devfrmendes@gmail.com>
2017-11-19 21:38:09 +0000

    Open the app to the world

--- config/initializers/basic_auth.rb
+++
@@ -1,8 +0,0 @@
-# TODO: remove this auth before release
-if %w(production staging).include?(Rails.env) && ENV["BASIC_AUTH"].present?
-  Rails.application.configure do
-    config.middleware.use Rack::Auth::Basic do |username, password|
-      [username, password] == ENV["BASIC_AUTH"].split(":")
-    end
-  end
-end
