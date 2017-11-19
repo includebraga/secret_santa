21d60520c6b94f5d56edf915b37a1ce56922d198
xufuou <xufuou@gmail.com>
2017-11-19 18:01:02 +0000

    test lets encrypt deactivate basic auth

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
   def letsencrypt
     # use your code here, not mine
-    render plain: "mkf7qP1qMFymkrBzINt7xMOISSh8ZgHtJbM4W1_PHm4.JQFRwkt-UbIROzZAlVQG8Fk4C9wiQ8Gv_d6wh9d33x8"
+    render plain: "7OUe-jRnPjz41w5VhHuTU0Idjy4fDpEtRKXCke3mBl4.JQFRwkt-UbIROzZAlVQG8Fk4C9wiQ8Gv_d6wh9d33x8"
   end
 end
