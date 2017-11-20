b6b221c7d0216a9d0b6d30e22dbf47cd2ea7a1e3
Fernando Mendes <devfrmendes@gmail.com>
2017-11-20 00:42:52 +0000

    Let the party start

--- config/initializers/auth.rb
+++
@@ -1,7 +0,0 @@
-if %w(production staging).include?(Rails.env) && ENV["BASIC_AUTH"].present?
-  Rails.application.configure do
-    config.middleware.use Rack::Auth::Basic do |username, password|
-      [username, password] == ENV["BASIC_AUTH"].split(":")
-    end
-  end
-end
