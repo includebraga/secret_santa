f4860f9bf6f44297248c2a21baa105a4145b4c50
José Sousa <josesousa9000@gmail.com>
2018-10-24 09:59:58 +0100

    wip

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,10 @@ module ApplicationHelper
       model.errors[attr].map(&:capitalize).join(". ")
     end
   end
+
+  # Shorthand helper for react components
+  # Only use prerender in test and prod environments, even if it's set to true
+  def react_component_helper(component:, prerender: false)
+    react_component(component, nil, prerender: Rails.env.development? ? false : prerender)
+  end
 end
