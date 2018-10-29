b8147bf89375c4c91fffd1a0867cfe0b1ee0144c
José Francisco Sousa <josesousa9000@gmail.com>
2018-10-29 10:57:03 +0000

    Add custom render_react_component helper
    This creates a new `render_react_component` method that acts as a shortcut to the `react_component` helper. It also only allows prerendering (server side rendering) outside of the development environment, because it's hard to debug server side rendering using Rails.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,10 @@ module ApplicationHelper
       model.errors[attr].map(&:capitalize).join(". ")
     end
   end
+
+  # Shorthand helper for react components
+  # Only use prerender in test and prod environments, even if it's set to true
+  def render_react_component(component:, prerender: false)
+    react_component(component, nil, prerender: Rails.env.development? ? false : prerender)
+  end
 end
