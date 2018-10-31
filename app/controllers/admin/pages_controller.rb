d12a786a0f1eab4a274e1c39e2e18552f77fc26e
José Sousa <josesousa9000@gmail.com>
2018-10-31 18:17:41 +0000

    Fix statistics page

--- app/controllers/admin/pages_controller.rb
+++ app/controllers/admin/pages_controller.rb
@@ -1,46 +1,30 @@
 module Admin
   class PagesController < Admin::ApplicationController
     def index
-      @remaining_receivers = remaining_receivers.count
-      @exceeding_receivers = exceeding_receivers.count
-      @users_without_redeem = users_without_redeem.count
       @gifts_received = gifts_received
       @gifts_matched = gifts_matched
-      @total_receivers = total_receivers
+      @gifts_missing = @gifts_matched - @gifts_received
+      @normal_receivers = normal_receivers
+      @golden_receivers = golden_receivers
+      @total_receivers = @normal_receivers + @golden_receivers
     end
 
     private
 
-    def remaining_receivers
-      Receiver.where(golden: false, matched_gifts: 1)
-    end
-
-    def exceeding_receivers
-      Receiver.where(golden: false, matched_gifts: 3)
-    end
-
     def gifts_received
-      Receiver.sum(:received_gifts)
+      Match.where(received: true).count
     end
 
     def gifts_matched
-      Receiver.sum(:matched_gifts)
+      Match.count
     end
 
-    def total_receivers
-      (normal_receiver_total * 2) + golden_receiver_total
-    end
-
-    def normal_receiver_total
+    def normal_receivers
       Receiver.where(golden: false).count
     end
 
-    def golden_receiver_total
+    def golden_receivers
       Receiver.where(golden: true).count
-    end
-
-    def users_without_redeem
-      User.where.not(redeem_token: nil).select { |u| u.matches.empty? }
     end
   end
 end
