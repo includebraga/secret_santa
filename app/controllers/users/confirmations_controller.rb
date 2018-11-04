312768775c1eecbd00e6d00f943c1ee56d100687
José Sousa <josesousa9000@gmail.com>
2018-11-04 02:17:54 +0000

    Immediatly show match upon user confirmation

--- app/controllers/users/confirmations_controller.rb
+++ app/controllers/users/confirmations_controller.rb
@@ -5,8 +5,20 @@ class Users::ConfirmationsController < ApplicationController
 
     if user_confirmation.successful?
       @user = user_confirmation.user
+      @receiver = match_assignment_receiver
+
+      not_found unless @receiver
     else
       not_found
     end
+  end
+
+  private
+
+  def match_assignment_receiver
+    match_assignment = MatchAssignment.new(@user)
+    match_assignment.perform
+
+    match_assignment.successful? ? match_assignment.receiver : nil
   end
 end
--- app/models/settings.rb
+++ app/models/settings.rb
@@ -7,7 +7,7 @@ class Settings < ApplicationRecord
   end
 
   def self.toggle_registrations
-    registrations_enabled = Settings.find_or_initialize_by(key: :registrations_enabled)
+    registrations_enabled = Settings.find_or_initialize_by(key: :REGISTRATIONS_ENABLED)
 
     registrations_enabled.value = case registrations_enabled.value
                                   when nil, "false"
