5aaccc62fd9078160bf88362f034752599d5a8f2
José Sousa <josesousa9000@gmail.com>
2018-11-07 15:20:12 +0000

    New route format

--- app/controllers/admin/institutions_controller.rb
+++ app/controllers/admin/institutions_controller.rb
@@ -1,12 +1,12 @@
 module Admin
   class InstitutionsController < Admin::ApplicationController
     def import_receivers
-      service = ImportReceiversFromCSV.new(path: params["file"].tempfile, institution_id: params[:id])
+      service = ImportReceiversFromCsv.new(path: params["file"].tempfile, institution_id: params[:institution_id])
       service.perform
 
       flash[:notice] = "Imported #{service.imported_receivers} receivers with #{service.failed_receivers} failures!"
 
-      redirect_to admin_institution_path(institution)
+      redirect_to admin_institution_path(params[:institution_id])
     end
   end
 end
