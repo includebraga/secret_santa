f5c7f074342fc44651c12aed19e3908156a464ba
José Sousa <josesousa9000@gmail.com>
2018-11-09 17:13:37 +0000

    Remove not needed controller actions

--- config/routes.rb
+++ config/routes.rb
@@ -20,13 +20,7 @@ Rails.application.routes.draw do
   namespace :admin do
     post "/confirm/:id", to: "users#confirm", as: :users_confirmation
     post "/confirm/", to: "users#batch_confirm", as: :users_batch_confirmation
-    post "/assign/:id", to: "users#assign", as: :users_assign
-    post "/assign", to: "users#batch_assign", as: :users_batch_assign
     post "/set_registrations", to: "users#set_registrations", as: :users_set_registrations
-    post "/match/:id", to: "users#match", as: :users_match
-    post "/match/", to: "users#batch_match", as: :users_batch_match
-    post "/receive/:id", to: "users#receive", as: :users_receive
-    post "/revert/:id", to: "users#revert_receive", as: :users_revert
     post "/institution/:institution_id/import_receivers", to: "institutions#import_receivers", as: :institutions_import_receivers
     post "/match/:match_id/received", to: "matches#received", as: :match_received
   end
