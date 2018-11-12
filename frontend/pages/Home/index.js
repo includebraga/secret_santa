2be8e0513809470248222381cc4aca87f5a8e913
José Sousa <josesousa9000@gmail.com>
2018-11-12 14:01:50 +0000

    Add 3rd section, use new layout defined by width

--- frontend/pages/Home/index.js
+++ frontend/pages/Home/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
 import Header from 'root/components/Header';
+import SignUp from 'root/components/SignUp';
 
 import './index.css';
 
 const HomePage = () => (
   <div styleName="root">
     <Header />
+    <SignUp />
   </div>
 );
 
