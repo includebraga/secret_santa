bbc525262d995d2995b39307bfef132349a66980
José Sousa <josesousa9000@gmail.com>
2018-11-18 22:38:34 +0000

    Tests

--- spec/services/match_assignment_spec.rb
+++ spec/services/match_assignment_spec.rb
@@ -12,8 +12,22 @@ RSpec.describe MatchAssignment, type: :model do
       end.to change { Match.count }.by(1)
 
       expect(match_assignment).to be_successful
-      expect(Match.first.receiver).to eq(receiver)
-      expect(Match.first.user).to eq(user)
+      expect(match_assignment.match.receiver).to eq(receiver)
+      expect(match_assignment.match.user).to eq(user)
+    end
+
+    it "creates a match for a given receiver" do
+      user = create(:user)
+      receiver = create_list(:receiver, 3).last
+      match_assignment = MatchAssignment.new(user, receiver)
+
+      expect do
+        match_assignment.perform
+      end.to change { Match.count }.by(1)
+
+      expect(match_assignment).to be_successful
+      expect(match_assignment.match.receiver).to eq(receiver)
+      expect(match_assignment.match.user).to eq(user)
     end
 
     it "is unsuccessful if there are no receivers" do
@@ -25,7 +39,7 @@ RSpec.describe MatchAssignment, type: :model do
       expect(match_assignment).not_to be_successful
     end
 
-    it "is unsuccesful if the user already has a match" do
+    it "is unsuccessful if the user already has a match" do
       user = create(:user)
       create(:receiver)
       create(:receiver)
@@ -59,6 +73,16 @@ RSpec.describe MatchAssignment, type: :model do
       match_assignment.perform
 
       expect(match_assignment.receiver).to eq(available_receiver)
+    end
+
+    it "is unsuccessful if receiver already has match" do
+      match = create(:match)
+      user = create(:user)
+      match_assignment = MatchAssignment.new(user, match.receiver)
+
+      match_assignment.perform
+
+      expect(match_assignment).to_not be_successful
     end
   end
 end
