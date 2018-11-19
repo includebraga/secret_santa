227bc673116f02e58c6498018f5c661541f06105
José Francisco Sousa <josesousa9000@gmail.com>
2018-11-19 11:52:20 +0000

    Use MatchAssignment service in the admin dashboard
    Why:
    
    * We need to create custom matches, and we needed the logic used in the match assignment
    
    This change addresses the need by:
    
    * Adding an optional `receiver` argument to the MatchAssignment service to create a match with that specific receiver.

--- app/controllers/admin/matches_controller.rb
+++ app/controllers/admin/matches_controller.rb
@@ -1,5 +1,18 @@
 module Admin
   class MatchesController < Admin::ApplicationController
+    def create
+      match_assigment = MatchAssignment.new(find_user)
+      match_assigment.perform
+
+      if match_assigment.successful?
+        flash[:notice] = "Match created!"
+        redirect_to admin_match_path(match_assigment.match)
+      else
+        flash[:error] = "Something went wrong."
+        redirect_to admin_matches_path
+      end
+    end
+
     def received
       match = Match.find(params[:match_id])
       match.update(received: true)
@@ -7,6 +20,20 @@ module Admin
       flash[:notice] = "Match marked as received!"
 
       redirect_to admin_matches_path
+    end
+
+    private
+
+    def find_user
+      User.find(match_params[:user_id])
+    end
+
+    def find_match
+      Match.find(match_params[:match_id])
+    end
+
+    def match_params
+      params.require(:match).permit(:user_id, :match_id)
     end
   end
 end
