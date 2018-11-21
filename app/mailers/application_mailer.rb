4316f6cfab5153bb08a837fc1d9c1e32e3ac1453
Fernando Mendes <fernando@mendes.codes>
2018-11-21 21:56:25 +0000

    Attempt to fix mailer

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -3,5 +3,6 @@ class ApplicationMailer < ActionMailer::Base
   FROM_EMAIL = Rails.application.secrets.mailer_from_email.freeze
 
   default from: "#{FROM_NAME} <#{FROM_EMAIL}>"
+  default "Message-ID" => -> { "<#{SecureRandom.uuid}@#{ENV['HOST_DOMAIN']}>" }
   layout "mailer"
 end
