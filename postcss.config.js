f43ca156f97ff6d3b47da22843338754c8ccca58
José Sousa <josesousa9000@gmail.com>
2018-11-30 21:42:23 +0000

    HOTFIX: Fix media queries

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,12 @@
 module.exports = {
   plugins: {
-    "precss": {},
-    "postcss-import": {},
-    "postcss-cssnext": {},
-    "postcss-modules": {
+    precss: {},
+    'postcss-import': {},
+    'postcss-cssnext': {},
+    'postcss-calc': { mediaQueries: true },
+    'postcss-modules': {
       getJSON: () => {},
-      generateScopedName: "[folder]__[local]__[hash]"
+      generateScopedName: '[folder]__[local]__[hash]',
     },
-  }
+  },
 };
