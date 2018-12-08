1325d91f2c6e35bf58b36f331b657208f6c4585a
José Sousa <josesousa9000@gmail.com>
2018-12-08 16:09:58 +0000

    HOTFIX: Add search by email to unreceived match

--- app/dashboards/unreceived_match_dashboard.rb
+++ app/dashboards/unreceived_match_dashboard.rb
@@ -13,7 +13,10 @@ class UnreceivedMatchDashboard < Administrate::BaseDashboard
   # which determines how the attribute is displayed
   # on pages throughout the dashboard.
   ATTRIBUTE_TYPES = {
-    user: Field::BelongsTo,
+    user: Field::BelongsTo.with_options(
+      searchable: true,
+      searchable_field: "email",
+    ),
     receiver: Field::BelongsTo,
     id: Field::Number,
     code: Field::String,
