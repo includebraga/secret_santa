5e15640a1e3dd07a5307ccb9aa61041e89c17048
José Francisco Sousa <josesousa9000@gmail.com>
2018-12-10 16:16:40 +0000

    Update receiver_spec.rb

--- spec/models/receiver_spec.rb
+++ spec/models/receiver_spec.rb
@@ -7,21 +7,21 @@ RSpec.describe Receiver, type: :model do
   it { should validate_presence_of(:letter) }
   it { should validate_presence_of(:institution_id) }
 
-  it "should have a gift" do
+  it "should have one gift if it only has one match" do
     receiver = create(:receiver)
     create(:match, receiver: receiver, received: true)
 
     expect(receiver.number_of_gifts).to eq(1)
   end
 
-  it "should have a gift" do
+  it "should have one gift if it only has one unreceived match" do
     receiver = create(:receiver)
     create(:unreceived_match, receiver: receiver, received: true)
 
     expect(receiver.number_of_gifts).to eq(1)
   end
 
-  it "should have a gift" do
+  it "should have a gift if it has both one match and unreceived match" do
     receiver = create(:receiver)
     create(:unreceived_match, receiver: receiver, received: true)
     create(:match, receiver: receiver, received: true)
